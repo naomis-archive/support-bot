5fe7082dd321a97387732ca6bd25476db003444d
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-07-28 10:28:51 -0700

    feat: add utils
    Set up logging and error handling.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("alive");
+import * as Sentry from "@sentry/node";
+import { RewriteFrames } from "@sentry/integrations";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+  integrations: [
+    new RewriteFrames({
+      root: global.__dirname,
+    }),
+  ],
+});
